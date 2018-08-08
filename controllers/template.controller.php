30808c5071f74bc29d7434cb323fc6d90e29efff
Emerson YR <emersonyuri32@gmail.com>
2018-08-08 17:33:17 -0500

    init project pos

---
+++ controllers/template.controller.php
@@ -0,0 +1,9 @@
+<?php
+
+class ControllerTemplate {
+	
+	public function crtTemplate() {
+		
+		include "views/template.php";
+	}
+}
---
+++ models/headboard.php
@@ -0,0 +1,24 @@
+<header class="main-header">
+
+	<!--======================================================
+	LOGOTIPO
+	=======================================================-->
+	<a href="" class="logo">
+	
+		<!-- logo mini -->
+		<span class="logo-mini">
+
+			<img src="views/img/template/icono-blanco.png" class="img-responsive" alt="Logo blanco" style="padding:10px;">
+
+		</span>
+
+		<!-- logo normal -->
+		<span class="logo-mini">
+
+			<img src="views/img/template/icono-blanco-lineal.png" class="img-responsive" alt="Logo blanco lineal" style="padding:10px 0px;">
+
+		</span>
+
+	</a>
+
+</header>
