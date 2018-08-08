7e805ac6616e6de7813e9b76d3edd6e1f963e01a
Emerson YR <emersonyuri32@gmail.com>
2018-08-08 18:03:02 -0500

    navbar header

--- models/headboard.php
+++ models/headboard.php
@@ -3,22 +3,49 @@
 	<!--======================================================
 	LOGOTIPO
 	=======================================================-->
+
 	<a href="" class="logo">
-	
 		<!-- logo mini -->
 		<span class="logo-mini">
-
 			<img src="views/img/template/icono-blanco.png" class="img-responsive" alt="Logo blanco" style="padding:10px;">
-
 		</span>
 
 		<!-- logo normal -->
-		<span class="logo-mini">
-
-			<img src="views/img/template/icono-blanco-lineal.png" class="img-responsive" alt="Logo blanco lineal" style="padding:10px 0px;">
-
+		<span class="logo-lg">
+			<img src="views/img/template/logo-blanco-lineal.png" class="img-responsive" alt="Logo blanco lineal" style="padding:10px 0px;">
 		</span>
-
 	</a>
+
+	<!--======================================================
+	BARRA DE NAVEGACIÓN
+	=======================================================-->
+
+	<nav class="navbar navbar-static-top" role="navigation">
+
+		<!-- Botón de navegación -->
+		<a href="" class="sidebar-toggle" data-toggle="push-menu" role="button">
+			<span class="sr-only">Toggle Navigation</span>
+		</a>
+
+		<!-- Perfil de usuario -->
+		<div class="navbar-custom-menu">
+			<ul class="nav navbar-nav">
+				<li class="dropdown user user-menu">
+					<a href="" class="dropdown-toggle" data-toggle="dropdown">
+						<img src="views/img/users/default/anonymous.png" alt="Imagen de usuario" class="user-image">
+						<span class="hidden-xs">Usuario Administrador</span>
+					</a>
+					<!-- Dropdown toggle -->
+					<ul class="dropdown-menu">
+						<li class="user-body">
+							<div class="pull-right"><a href="" class="btn btn-default btn-flat">Salir</a></div>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+
+
+	</nav>
 
 </header>
