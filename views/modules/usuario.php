212a2b58af7b50cb5f371a3e956cfd8687239d5c
Emerson YR <emersonyuri32@gmail.com>
2018-08-14 20:53:51 -0500

    maquetado de modulo de usuarios - parte1

--- views/modules/usuario.php
+++ views/modules/usuario.php
@@ -16,28 +16,99 @@
       <!-- Default box -->
       <div class="box">
         <div class="box-header with-border">
-          <h3 class="box-title">Title</h3>
-
-          <div class="box-tools pull-right">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
-                    title="Collapse">
-              <i class="fa fa-minus"></i></button>
-            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-              <i class="fa fa-times"></i></button>
-          </div>
+            <button class="btn btn-primary" data-toggle="modal" data-target="#modalAgregarUsuario">
+                Agregar usuario
+            </button>
         </div>
         <div class="box-body">
-          Start creating your amazing application!
+            <table class="table table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Nombre</th>
+                        <th>Usuario</th>
+                        <th>Foto</th>
+                        <th>Perfil</th>
+                        <th>Estado</th>
+                        <th>Último login</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Usuario Administrador</td>
+                        <td>Admin</td>
+                        <td><img src="views/img/users/default/anonymous.png" class="img-thumbnail" width="40px" alt="Usuario por defecto"></td>
+                        <td>Administrador</td>
+                        <td><button class="btn btn-success btn-xs">Activado</button></td>
+                        <td>2018-08-10 12:05:03</td>
+                        <td>
+                            <div class="btn-group">
+                                <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                                <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Usuario Administrador</td>
+                        <td>Admin</td>
+                        <td><img src="views/img/users/default/anonymous.png" class="img-thumbnail" width="40px" alt="Usuario por defecto"></td>
+                        <td>Administrador</td>
+                        <td><button class="btn btn-success btn-xs">Activado</button></td>
+                        <td>2018-08-10 12:05:03</td>
+                        <td>
+                            <div class="btn-group">
+                                <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                                <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Usuario Administrador</td>
+                        <td>Admin</td>
+                        <td><img src="views/img/users/default/anonymous.png" class="img-thumbnail" width="40px" alt="Usuario por defecto"></td>
+                        <td>Administrador</td>
+                        <td><button class="btn btn-danger btn-xs">Desactivado</button></td>
+                        <td>2018-08-10 12:05:03</td>
+                        <td>
+                            <div class="btn-group">
+                                <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                                <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
         <!-- /.box-body -->
-        <div class="box-footer">
-          Footer
-        </div>
-        <!-- /.box-footer-->
       </div>
       <!-- /.box -->
 
     </section>
     <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
+</div>
+<!-- /.content-wrapper -->
+
+<!--======================================================
+MODAL AGREGAR USUARIO
+=======================================================-->
+
+<div id="modalAgregarUsuario" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button class="close" type="button" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Modal Header</h4>
+            </div>
+            <div class="modal-body">
+                <p>Some text in the model.</p>
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-default" type="button" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
