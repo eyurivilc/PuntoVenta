165dabaa4870ae27791ab71b1e5d047620472094
Emerson YR <emersonyuri32@gmail.com>
2018-08-16 17:04:42 -0500

    editar usuarios parte 1

--- views/modules/usuarios.php
+++ views/modules/usuarios.php
@@ -59,7 +59,7 @@
                                 <td>'.$value["ultimo_login"].'</td>
                                 <td>
                                     <div class="btn-group">
-                                        <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                                        <button class="btn btn-warning btnEditarUsuario" idUsuario="'.$value["id"].'" data-toggle="modal" data-target="#modalEditarUsuario"><i class="fa fa-pencil"></i></button>
                                         <button class="btn btn-danger"><i class="fa fa-times"></i></button>
                                     </div>
                                 </td>
@@ -143,6 +143,72 @@ MODAL AGREGAR USUARIO
                     $crearUsuario = new ControllerUsuarios();
                     $crearUsuario -> crtCrearUsuario();
                 ?>
+            </form>
+        </div>
+    </div>
+</div>
+
+<!--======================================================
+MODAL EDITAR USUARIO
+=======================================================-->
+
+<div id="modalEditarUsuario" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <form role="form" method="post" enctype="multipart/form-data">
+                <div class="modal-header" style="background: #3c8dbc; color: #FFF;">
+                    <button class="close" type="button" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Editar usuario</h4>
+                </div>
+                <div class="modal-body">
+                    <div class="box-body">
+                        <!--Entrada para el nombre-->
+                        <div class="form-group">
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                                <input type="text" class="form-control input-lg" id="editarNombre" name="editarNombre" value="" required>
+                            </div>
+                        </div>
+                        <!--Entrada para el usuario-->
+                        <div class="form-group">
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="fa fa-key"></i></span>
+                                <input type="text" class="form-control input-lg" id="editarUsuario" name="editarUsuario" value="" required>
+                            </div>
+                        </div>
+                        <!--Entrada para el password-->
+                        <div class="form-group">
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+                                <input type="password" class="form-control input-lg" name="editarPassword" placeholder="Escriba la nueva contraseña" required>
+                            </div>
+                        </div>
+                        <!--Entrada para el seleccionar perfil-->
+                        <div class="form-group">
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="fa fa-users"></i></span>
+                                <select name="editarPerfil" class="form-control input-lg">
+                                    <option value="" id="editarPerfil"></option>
+                                    <option value="Administrador">Administrador</option>
+                                    <option value="Especial">Especial</option>
+                                    <option value="Vendedor">Vendedor</option>
+                                </select>
+                            </div>
+                        </div>
+                        <!--Entrada para el subir foto-->
+                        <div class="form-group">
+                            <div class="panel">SUBIR FOTO</div>
+                            <input type="file" class="nuevaFoto" name="editarFoto">
+                            <p class="help-block">Peso máximo de la foto 2MB</p>
+                            <img src="views/img/users/default/anonymous.png" class="img-thumbnail previsualizar" width="100px" alt="Foto por defecto">
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Salir</button>
+                    <button type="submit" class="btn btn-primary">Modificar usuario</button>
+                </div>
+
             </form>
         </div>
     </div>
